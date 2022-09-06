526339090e581076a8f806c7df0d0a92be7b4ccd
Gabriel Aillapan <g.aillapan01@ufromail.cl>
2022-09-06 16:42:32 -0400

    Spring Framework agregado

---
+++ laligapremier/src/main/java/com/proyecto/laligapremier/LaligapremierApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.laligapremier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaligapremierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaligapremierApplication.class, args);
+	}
+
+}
---
+++ laligapremier/src/test/java/com/proyecto/laligapremier/LaligapremierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.laligapremier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaligapremierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

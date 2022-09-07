57330efcc7bd7ea618e2ddb3e68cc55cf97605f5
Gabriel Aillapan <g.aillapan01@ufromail.cl>
2022-09-07 15:18:21 -0400

    Package service - Metodos implementados de CamisetaService

--- laligapremier/src/main/java/com/proyecto/laligapremier/service/CamisetaServiceImpl.java
+++ laligapremier/src/main/java/com/proyecto/laligapremier/service/CamisetaServiceImpl.java
@@ -1,22 +1,40 @@
 package com.proyecto.laligapremier.service;
 
 import com.proyecto.laligapremier.models.dao.ICamisetaDao;
+import com.proyecto.laligapremier.models.entity.Camiseta;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Map;
-
+import java.util.List;
 @Service
-public class CamisetaServiceImpl {
+public class CamisetaServiceImpl implements ICamisetaService {
 
     @Autowired
     private ICamisetaDao camisetaDao;
 
-    public String listar(@RequestParam(value = "id") Long id, Map<String , Object> model , RedirectAttributes flash){
-        
-        return "listar";
+    @Override
+    public List<Camiseta> findAll() {
+        return (List<Camiseta>) camisetaDao.findAll();
     }
 
+    @Override
+    public Page<Camiseta> findAll(Pageable pageable) {
+        return camisetaDao.findAll(pageable);
+    }
+
+    @Override
+    public void delete(Long id) {
+        camisetaDao.deleteById(id);
+    }
+
+    @Override
+    public Camiseta findOne(Long id) {
+        return camisetaDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public void save(Camiseta camiseta) {
+        camisetaDao.save(camiseta)
+    }
 }

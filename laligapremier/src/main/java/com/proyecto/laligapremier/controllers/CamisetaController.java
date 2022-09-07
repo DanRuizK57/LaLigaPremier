0dbae97326fd14826ddc4ca80e7a89104429f02e
Gabriel Aillapan <g.aillapan01@ufromail.cl>
2022-09-07 16:16:59 -0400

    POMXML - thymeleaf | Package Controller - Metodo index

--- laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
+++ laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
@@ -1,13 +1,25 @@
 package com.proyecto.laligapremier.controllers;
 
+import com.proyecto.laligapremier.service.ICamisetaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("camiseta")
 public class CamisetaController {
+    @Autowired
+    private ICamisetaService camisetaService;
 
-    @RequestMapping(name = "/index" )
-    public String index(){
+    @RequestMapping(name = "/"  , method = RequestMethod.GET)
+    public String index(Model model){
+        model.addAttribute("titulo" , "Listado de camisetas");
+        model.addAttribute("camisetas" , camisetaService.findAll() );
         return "index";
     }
+
+
 }

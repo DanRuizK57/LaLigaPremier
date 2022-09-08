9a22851c63be2f9795297c5293acb85a995fa820
Gabriel Aillapan <g.aillapan01@ufromail.cl>
2022-09-07 21:48:23 -0400

    Templates - vista selecciones y equipos creada

--- laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
+++ laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
@@ -3,6 +3,7 @@ import com.proyecto.laligapremier.service.ICamisetaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -13,20 +14,14 @@ public class CamisetaController {
     @Autowired
     private ICamisetaService camisetaService;
 
-    @RequestMapping(name = "/"  , method = RequestMethod.GET)
-    public String index(Model model){
-        model.addAttribute("titulo" , "Listado de camisetas");
-        model.addAttribute("camisetas" , camisetaService.findAll() );
-        return "index";
-    }
-
-    @RequestMapping(name = "/selecciones" , method = RequestMethod.GET)
+    @GetMapping(value = "/selecciones")
     public String selecciones(Model model){
         model.addAttribute("titulo" , "Listado de camisetas de selecciones");
         model.addAttribute("camisetasSelecciones" , camisetaService.findAll());
         return "selecciones";
     }
 
+    @GetMapping(value = "/equipos")
     public String equipos(Model model){
         model.addAttribute("titulo" , "Listaod de camisetas de equipos");
         model.addAttribute("camisetasEquipos" , camisetaService.findAll());

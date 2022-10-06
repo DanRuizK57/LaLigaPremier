03920f9db1afb41404bc28369a899c373683f3e5
Gabriel Aillapan <g.aillapan01@ufromail.cl>
2022-10-06 18:45:53 -0300

    Metodo verCamiseta  implementado + cambio de nombre de los metodos

--- laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
+++ laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
@@ -1,10 +1,17 @@
 package com.proyecto.laligapremier.controllers;
+import com.proyecto.laligapremier.models.entity.Camiseta;
 import com.proyecto.laligapremier.service.ICamisetaService;
+
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @SessionAttributes("camiseta")
@@ -13,27 +20,40 @@ public class CamisetaController {
     private ICamisetaService camisetaService;
 
     @GetMapping(value = "/selecciones")
-    public String selecciones(Model model){
+    public String ListarSelecciones(Model model){
         model.addAttribute("titulo" , "Listado de camisetas de selecciones");
         model.addAttribute("camisetasSelecciones" , camisetaService.findAll());
         return "mostrar/selecciones";
     }
 
     @GetMapping(value = "/equipos")
-    public String equipos(Model model){
+    public String ListarEquipos(Model model){
         model.addAttribute("titulo" , "Listado de camisetas de equipos");
         model.addAttribute("camisetasEquipos" , camisetaService.findAll());
         return "mostrar/equipos";
     }
 
     @GetMapping(value = "/ver-camiseta")
-    public String verCamiseta(Model model){
+    public String verCamiseta(@PathVariable(value = "id")Long id, Map <String, Object> model , RedirectAttributes flash){
+        Camiseta camiseta = camisetaService.findOne(id);
+        if(camiseta == null){
+            flash.addAttribute("error" , "Camiseta no encontrada");
+            return "redirect:/mostrar/equipos";
+        }
+        model.put("camiseta", camiseta);
+        model.put("titulo", "detalle de la camiseta ");
         return "camiseta/ver_camiseta";
     }
 
     @GetMapping(value = "/formulario")
-    public String formulario(Model model){
-        return "camiseta/form_camiseta";
+    public String formulario(Map<String , Object>model){
+
+        Camiseta camiseta = new Camiseta(); 
+        model.put("camiseta" , camiseta); 
+        return "camiseta/form_camiseta"; 
+    
     }
+
+  
 
 }

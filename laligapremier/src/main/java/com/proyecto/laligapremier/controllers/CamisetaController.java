d7c0647e78d8fb453c6353709e2a7eb861d3d5ac
Gabriel Aillapan <g.aillapan01@ufromail.cl>
2022-10-06 23:07:27 -0300

    Metodo avanzado en controller camiseta - application propiertes modificado para postgresql

--- laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
+++ laligapremier/src/main/java/com/proyecto/laligapremier/controllers/CamisetaController.java
@@ -1,16 +1,19 @@
 package com.proyecto.laligapremier.controllers;
 import com.proyecto.laligapremier.models.entity.Camiseta;
 import com.proyecto.laligapremier.service.ICamisetaService;
-
-import java.lang.ProcessBuilder.Redirect;
 import java.util.Map;
-
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -33,6 +36,14 @@ public class CamisetaController {
         return "mostrar/equipos";
     }
 
+    /**
+     * Metodo controlador encargado de la buscar una camiseta consultada por el id
+     * Metodo que hay que revisar, debido a que puede tener multiples vistas dependiendo de la camiseta
+     * @param id
+     * @param model
+     * @param flash
+     * @return vista que muestra las camisetas
+     */
     @GetMapping(value = "/ver-camiseta")
     public String verCamiseta(@PathVariable(value = "id")Long id, Map <String, Object> model , RedirectAttributes flash){
         Camiseta camiseta = camisetaService.findOne(id);
@@ -45,14 +56,33 @@ public class CamisetaController {
         return "camiseta/ver_camiseta";
     }
 
-    @GetMapping(value = "/formulario")
-    public String formulario(Map<String , Object>model){
-
+    @RequestMapping(value = "/camiseta/form_camiseta")
+    public String crearCamiseta(Map<String , Object>model){    
         Camiseta camiseta = new Camiseta(); 
         model.put("camiseta" , camiseta); 
+        model.put("titulo", "Agregar Camiseta");
         return "camiseta/form_camiseta"; 
     
     }
+    public String guardar(
+        @Valid Camiseta camiseta,
+        BindingResult result,
+        Model model,
+        @RequestParam("file") MultipartFile imagen,
+        RedirectAttributes flash,
+        SessionStatus status
+    ){
+        if(result.hasErrors()){
+            model.addAttribute("titulo" , "Agregar camiseta"); 
+        }
+        if(!imagen.isEmpty()){
+            //falta estudio para implementar la subida de imagen correctamente
+        }
+
+        return "redirect:/mostrar/index_admin"; 
+    }
+
+    
 
   
 

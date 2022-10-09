cf0ebe6d4e8c4a743bc24a7c6c6bbeb65bf359d9
GabeASM <g.aillapan01@gmail.com>
2022-10-09 19:18:07 -0300

    inicializando directorio uploads automaticamente con CommandLineRunner

--- laligapremier/src/main/java/com/proyecto/laligapremier/LaligapremierApplication.java
+++ laligapremier/src/main/java/com/proyecto/laligapremier/LaligapremierApplication.java
@@ -1,13 +1,23 @@
 package com.proyecto.laligapremier;
 
+import com.proyecto.laligapremier.service.IUploadFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LaligapremierApplication {
+public class LaligapremierApplication implements CommandLineRunner {
+	@Autowired
+	IUploadFileService uploadFileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaligapremierApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		uploadFileService.init();
+	}
 }
--- laligapremier/src/main/java/com/proyecto/laligapremier/service/UploadFileServiceImpl.java
+++ laligapremier/src/main/java/com/proyecto/laligapremier/service/UploadFileServiceImpl.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -53,6 +54,17 @@ public class UploadFileServiceImpl implements IUploadFileService {
             if(archivo.delete()) return true;
         }
         return false;
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(Paths.get(UPLOAD_FOLDER).toFile());
+    }
+
+    @Override
+    public void init() throws IOException {
+        Files.createDirectory(Paths.get(UPLOAD_FOLDER));
+
     }
 
     public Path getPath(String filename){
